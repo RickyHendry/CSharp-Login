92f2828835a2978ddac86b3b0c3a0c3633ec05aa
Hendri <rickyhendry05@gmail.com>
2022-10-17 01:47:39 +0800

    Login

---
+++ SIBKMNET_MVCWeb/Context/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIBKMNET_MVCWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_MVCWeb.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+        // mengatur connection string (done)
+        // menambahkan model untuk diolah dan / atau migrasi
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ SIBKMNET_MVCWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SIBKMNET_MVCWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
